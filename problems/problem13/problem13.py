176c2eb23a917802841350e49ccca78f788494c0
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-07-26 19:28:23 -0400

    Update problem13.py

--- problems/problem13/problem13.py
+++ problems/problem13/problem13.py
@@ -1,4 +1,4 @@
-#file = file containing all 50-digit numbers, separated by a line break (\n)
+#file = file containing all 50-digit numbers, separated by a line break (\n) => prob13.txt in the problem13 folder
 def problem13(file):
     with open(file) as f:    
         numbers = [int(line[:-7]) for line in f.readlines() if line[:-7] != '']

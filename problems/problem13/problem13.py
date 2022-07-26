638ccc9655df46dfbf015c188d0e2f02567ffc63
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-07-26 19:26:02 -0400

    Create problem13.py

---
+++ problems/problem13/problem13.py
@@ -0,0 +1,6 @@
+#file = file containing all 50-digit numbers, separated by a line break (\n)
+def problem13(file):
+    with open(file) as f:    
+        lines = [int(line[:-7]) for line in f.readlines() if line[:-7] != '']
+        
+    return str(sum(lines))[:10]

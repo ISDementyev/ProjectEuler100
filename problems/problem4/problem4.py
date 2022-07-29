007dcf60b3a88d54613584fcb4682096fe5aad05
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-07-28 22:55:49 -0400

    Made main code more succint

--- problems/problem4/problem4.py
+++ problems/problem4/problem4.py
@@ -1,9 +1,6 @@
 def is_palindrome(n):
     n = str(n)
-    
     return n[::-1] == n
     
 def problem_4():
-    palindromes = [i * j for i in range(1, 1000) for j in range(1, 1000) if is_palindrome(i * j)]
-    
-    return max(palindromes)
+    return max([i * j for i in range(1, 1000) for j in range(1, 1000) if is_palindrome(i * j)])

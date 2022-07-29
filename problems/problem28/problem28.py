5d3633f4112ec7a971f4b86710589866a6f4e517
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-07-28 21:23:32 -0400

    Update problem28.py

--- problems/problem28/problem28.py
+++ problems/problem28/problem28.py
@@ -1,3 +1,5 @@
+from numba import jit
+
 # unoptimized
 def Problem28(n):
     n = (n - 1) // 2
@@ -17,5 +19,26 @@ def Problem28(n):
             diagonals.append(start)
                 
     return sum(diagonals) + 1 # returns sum(diagonals) + 1 since diagonal generation does not include 1x1 case
+
+@jit
+def Problem28J(n):
+    n = (n - 1) // 2
+    
+    # generate diagonal values:
+    diagonals = [3]
+    start = 3
+    
+    for i in range(1, n + 1):
+        if i == 1:
+            times = 2
+        else:
+            times = 3
+            
+        for _ in range(times + 1):
+            start += i * 2
+            diagonals.append(start)
+                
+    return sum(diagonals) + 1
   
 print(Problem28(1001))
+print(Problem28J(1001)) # roughly 47x faster

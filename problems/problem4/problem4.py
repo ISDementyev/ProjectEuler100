111a8b01cfb56e181941191e1ae3bfeb8d0a066f
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-07-29 12:30:37 -0400

    Update problem4.py

--- problems/problem4/problem4.py
+++ problems/problem4/problem4.py
@@ -1,6 +1,20 @@
+from numba import jit
+
 def is_palindrome(n):
     n = str(n)
     return n[::-1] == n
+
+@jit
+def is_palindromeJ(n):
+    n = str(n)
+    
+    return n[::-1] == n
+
+@jit
+def problem_4_jit(): # roughly twice as fast
+    palindromes = [i * j for i in range(1, 1000) for j in range(i, 1000) if is_palindromeJ(i * j)]
+    
+    return max(palindromes)
     
 def problem_4():
-    return max([i * j for i in range(1, 1000) for j in range(1, 1000) if is_palindrome(i * j)])
+    return max([i * j for i in range(1, 1000) for j in range(i, 1000) if is_palindrome(i * j)])

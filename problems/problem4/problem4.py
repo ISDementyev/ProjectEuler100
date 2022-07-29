51e77289d0d559e8e7d2103671bd19d7b12b761d
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-07-28 22:52:20 -0400

    Fixed typo

--- problems/problem4/problem4.py
+++ problems/problem4/problem4.py
@@ -1,7 +1,7 @@
 def is_palindrome(n):
     n = str(n)
     
-    return n[::-1] == n:
+    return n[::-1] == n
     
 def problem_4():
     palindromes = [i * j for i in range(1, 1000) for j in range(1, 1000) if is_palindrome(i * j)]

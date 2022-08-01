69ab3ea8823fd8d315611797f3f65b02a8a66fc1
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-07-31 22:26:26 -0400

    Added some optimized code
    Will add Numpy functions in the near future

--- problems/problem34/problem34.py
+++ problems/problem34/problem34.py
@@ -1,5 +1,19 @@
+from numba import jit
+
 # helpers
 def Factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    
+    Fac = 1
+    
+    for i in range(1, n + 1):
+        Fac *= i
+        
+    return Fac
+
+@jit
+def FactorialJ(n):
     if n == 0 or n == 1:
         return 1
     
@@ -25,4 +39,37 @@ def Problem34(n):
             
     return sum(ProblemList)
   
-  Problem34(int(1e6))
+# main
+# slightly faster (~500 ms less)
+def Problem34Simpler(n):
+    Total = 0
+    
+    for i in range(10, n + 1):
+        FacSum = 0
+        
+        for Digit in str(i):
+            FacSum += Factorial(int(Digit))
+        
+        if FacSum == i:
+            Total += i
+            
+    return Total
+
+# main
+# more optimized
+# 2.3x faster than original
+def Problem34J(n):
+    Total = 0
+    
+    for i in range(10, n + 1):
+        FacSum = 0
+        
+        for Digit in str(i):
+            FacSum += FactorialJ(int(Digit))
+        
+        if FacSum == i:
+            Total += i
+            
+    return Total
+
+Problem34J(1000000)

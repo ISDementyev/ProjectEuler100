61c328239e8ec40637caa787dd7f8d7b2501ba84
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-08-03 11:15:41 -0400

    Added simpler function and problem number typo

--- problems/problem2/Problem2.py
+++ problems/problem2/Problem2.py
@@ -8,12 +8,27 @@ def FibonacciBinet(N):
     return round((phi ** N - phiN ** N) / sqrt(5))
 
 # main - currently unoptimized
-def Problem9():
+def Problem2():
     i = 2
 
     while (FibonacciBinet(i) < 4_000_000):
         i += 1
 
     return sum([j for j in Fibonacci(i - 1) if j % 2 == 0])
+
+# More simpler and easier to read, but not much faster
+def Problem2Simpler():
+    i = 2
+    Sum = 0
+    
+    while (FibonacciBinet(i) < 4_000_000):
+        Term = FibonacciBinet(i)
+        
+        if Term % 2 == 0:
+            Sum += Term
+            
+        i += 1
+    
+    return Sum
   
-Problem9()
+Problem2Simpler()
